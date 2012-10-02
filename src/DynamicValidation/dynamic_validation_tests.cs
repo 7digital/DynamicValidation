44faccc90792f3c84c0dfc6dc2f9f00df21c8b56
Iain Ballard <ieb@me.com>
2012-10-02 20:12:10 +0100

    Shell

---
+++ src/DynamicValidation/dynamic_validation_tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DynamicValidation {
+	[TestFixture]
+	public class dynamic_validation_tests {
+
+		[Test]
+		public void quick_test () {
+
+		}
+	}
+}
