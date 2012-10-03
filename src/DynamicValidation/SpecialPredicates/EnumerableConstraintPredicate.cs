0f658b424ed9f6ab3ffe740b0d19cd40b4c97979
Iain Ballard <iain.ballard@7digital.com>
2012-10-03 14:33:55 +0100

    Cleanup

---
+++ src/DynamicValidation/SpecialPredicates/EnumerableConstraintPredicate.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace DynamicValidation
+{
+	public class EnumerableConstraintPredicate : INamedPredicate
+	{
+		readonly IResolveConstraint constraint;
+
+		public EnumerableConstraintPredicate(IResolveConstraint constraint)
+		{
+			this.constraint = constraint;
+		}
+
+		public bool Matches(object actual, out string message)
+		{
+			message = "";
+			var result = true;
+			var check = constraint.Resolve();
+			foreach (var obj in ((IEnumerable<object>)actual))
+			{
+
+				if (check.Matches(obj))
+				{
+					continue;
+				}
+
+				result = false;
+				var sw = new TextMessageWriter();
+				check.WriteMessageTo(sw);
+				message += sw+"\r\n";
+			}
+			return result;
+		}
+	}
+}
